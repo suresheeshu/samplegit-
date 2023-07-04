9243433bb9403268a6c8fc283178ab2155899ba8
suresheeshan <suresh.nssy9@gmail.com>
2023-07-04 22:53:10 +0530

    updated code

---
+++ src/main/java/com/example/demo/SamplegitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplegitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplegitApplication.class, args);
+	}
+
+}
